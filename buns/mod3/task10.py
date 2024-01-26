dca6303ab718e0d2a9d582923595010b082e6f78
toshagbs <129325399+toshagbs@users.noreply.github.com>
2024-01-26 07:40:38 +0500

    Update task10.py

--- buns/mod3/task10.py
+++ buns/mod3/task10.py
@@ -1,1 +1,18 @@
+size = int(input("Введите размерность квадратной матрицы: "))
 
+matrix = []
+for i in range(size):
+    row = [j + 1 for j in range(size)]
+    matrix.append(row)
+
+print("Исходная матрица:")
+for row in matrix:
+    print(row)
+
+for i in range(size):
+    for j in range(i + 1, size):
+        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+print("Транспонированная матрица:")
+for row in matrix:
+    print(row)
