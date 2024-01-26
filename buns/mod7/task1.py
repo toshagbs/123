38710cae881a8d35cd801d0338ba3e75860e0adf
toshagbs <129325399+toshagbs@users.noreply.github.com>
2024-01-26 07:50:48 +0500

    Update and rename task1 to task1.py

---
+++ buns/mod7/task1.py
@@ -0,0 +1,11 @@
+def validate_args(func):
+    def wrapper(*args, **kwargs):
+        if len(args) != 1:
+            return "Not enough arguments" if len(args) < 1 else "Too many arguments"
+        arg = args[0]
+        if not isinstance(arg, int):
+            return "Wrong types"
+        if arg < 0:
+            return "Negative argument"
+        return func(*args, **kwargs)
+    return wrapper
